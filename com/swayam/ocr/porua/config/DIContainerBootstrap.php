d7a2db35674141481c4b376074990aecc002ee39
Palash Ray <paawak@gmail.com>
2020-10-14 15:27:24 +0530

    have remote server config

--- com/swayam/ocr/porua/config/DIContainerBootstrap.php
+++ com/swayam/ocr/porua/config/DIContainerBootstrap.php
@@ -9,7 +9,16 @@ require __DIR__ . '/../../../../../vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder;
 $containerBuilder->addDefinitions(__DIR__ . '/DIConfiguration.php');
-$containerBuilder->addDefinitions(__DIR__ . '/LocalApplicationConfig.php');
+
+$customConfiguration = $_SERVER['HTTP_PORUA_APPLICATION_CONFIG'];
+
+if (($_SERVER['SERVER_NAME'] == 'ocrservice.paawak.me') 
+        && isset($customConfiguration)) {
+    $containerBuilder->addDefinitions('/home/users/web/b2928/ipg.paawak20736/dev/RemoteApplicationConfig.php');
+} else {
+    $containerBuilder->addDefinitions(__DIR__ . '/LocalApplicationConfig.php');
+}
+
 $container = $containerBuilder->build();
 
 return $container;
