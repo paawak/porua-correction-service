f43acacbab4a415ac89602cd6e60008f3bba21c9
Palash Ray <paawak@users.noreply.github.com>
2020-10-15 01:33:58 +0530

    Fixed google auth not working on Apache server
    Fixed google auth not working on Apache server

--- com/swayam/ocr/porua/config/DIContainerBootstrap.php
+++ com/swayam/ocr/porua/config/DIContainerBootstrap.php
@@ -10,10 +10,8 @@ require __DIR__ . '/../../../../../vendor/autoload.php';
 $containerBuilder = new ContainerBuilder;
 $containerBuilder->addDefinitions(__DIR__ . '/DIConfiguration.php');
 
-$customConfiguration = $_SERVER['HTTP_PORUA_APPLICATION_CONFIG'];
-
-if (($_SERVER['SERVER_NAME'] == 'ocrservice.paawak.me') 
-        && isset($customConfiguration)) {
+if ((getenv('SERVER_NAME') == 'ocrservice.paawak.me') 
+        && getenv("HTTP_PORUA_APPLICATION_CONFIG") !== false) {
     $containerBuilder->addDefinitions('/home/users/web/b2928/ipg.paawak20736/dev/RemoteApplicationConfig.php');
 } else {
     $containerBuilder->addDefinitions(__DIR__ . '/LocalApplicationConfig.php');
