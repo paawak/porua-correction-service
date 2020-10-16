14376c01a9ec8c8d619f5260bf93706b04402def
Palash Ray <paawak@gmail.com>
2020-10-16 17:44:38 +0530

    log errors, exceptions

--- com/swayam/ocr/porua/config/DIConfiguration.php
+++ com/swayam/ocr/porua/config/DIConfiguration.php
@@ -7,6 +7,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Processor\IntrospectionProcessor;
+use Monolog\ErrorHandler;
 use Psr\Log\LoggerInterface;
 use Psr\Container\ContainerInterface;
 
@@ -20,12 +21,15 @@ return [
         $fileHandler = new RotatingFileHandler($container->get('logger.fileName'), $container->get('logger.maxFiles'));
         $fileHandler->setFormatter($formatter);
         $logger->pushHandler($fileHandler);
-        
+
         $consoleHandler = new StreamHandler('php://stdout', Logger::DEBUG);
         $consoleHandler->setFormatter($formatter);
-        $logger->pushHandler($consoleHandler); 
-        
+        $logger->pushHandler($consoleHandler);
+
         $logger->pushProcessor(new IntrospectionProcessor());
+
+        ErrorHandler::register($logger);
+
         return $logger;
     },
     EntityManager::class => function (ContainerInterface $container) {
