35255890d037b4660babf1c9e6b0b98b42eb06e2
Palash Ray <paawak@gmail.com>
2021-02-14 02:25:34 +0530

    remove un-used imports

--- com/swayam/ocr/porua/model/OcrWordEntityTemplate.php
+++ com/swayam/ocr/porua/model/OcrWordEntityTemplate.php
@@ -8,9 +8,6 @@ use Doctrine\ORM\Mapping\Embedded;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\OneToMany;
-use Doctrine\ORM\Mapping\ManyToMany;
-use Doctrine\ORM\Mapping\JoinTable;
-use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Column;
 
