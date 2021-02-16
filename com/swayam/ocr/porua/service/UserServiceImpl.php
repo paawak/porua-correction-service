4267851c5a7253ee4a5468e6feb63bfc9a2fe8a4
Palash Ray <paawak@gmail.com>
2021-02-16 09:34:28 +0530

    new user registration

--- com/swayam/ocr/porua/service/UserServiceImpl.php
+++ com/swayam/ocr/porua/service/UserServiceImpl.php
@@ -5,9 +5,11 @@ namespace com\swayam\ocr\porua\service;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManager;
 use com\swayam\ocr\porua\model\UserDetails;
+use com\swayam\ocr\porua\model\UserRole;
 
 require_once __DIR__ . '/UserService.php';
 require_once __DIR__ . '/../model/UserDetails.php';
+require_once __DIR__ . '/../model/UserRole.php';
 
 /**
  *
@@ -31,7 +33,14 @@ class UserServiceImpl implements UserService {
     }
 
     public function registerNewUser(array $payload): UserDetails {
-        return null;
+        $userDetails = new UserDetails();
+        $userDetails->setName($payload['name']);
+        $userDetails->setEmail($payload['email']);
+        $userDetails->setRole(UserRole::CORRECTION_ROLE);
+        $this->entityManager->persist($userDetails);
+        $this->entityManager->flush();
+        $this->logger->info("Registered a New User:", array($userDetails));
+        return $userDetails;
     }
 
 }
