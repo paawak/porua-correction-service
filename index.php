d7e6ee2a09e15d7ed764b9e054e7a5bc7cf334a7
Palash Ray <paawak@gmail.com>
2021-02-20 20:27:01 +0530

    error reporting

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__ . '/com/swayam/ocr/porua/rest/IndexController.php';
 require __DIR__ . '/com/swayam/ocr/porua/rest/OCRQueryController.php';
 require __DIR__ . '/com/swayam/ocr/porua/rest/OCRCorrectionController.php';
