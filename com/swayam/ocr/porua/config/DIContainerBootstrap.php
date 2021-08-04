c300e7d09dc392cdf81d5cf3f2b73f40c8b73c2d
Palash Ray <paawak@gmail.com>
2021-08-05 00:50:36 +0530

    migrate to palashray.com

--- com/swayam/ocr/porua/config/DIContainerBootstrap.php
+++ com/swayam/ocr/porua/config/DIContainerBootstrap.php
@@ -10,7 +10,7 @@ require __DIR__ . '/../../../../../vendor/autoload.php';
 $containerBuilder = new ContainerBuilder;
 $containerBuilder->addDefinitions(__DIR__ . '/DIConfiguration.php');
 
-if ((getenv('SERVER_NAME') == 'ocrservice.paawak.me') 
+if ((getenv('SERVER_NAME') == 'ocrservice.palashray.com') 
         && getenv("HTTP_PORUA_APPLICATION_CONFIG") !== false) {
     $containerBuilder->addDefinitions('/home/users/web/b2928/ipg.paawak20736/dev/RemoteApplicationConfig.php');
 } else {
